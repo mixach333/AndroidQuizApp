cac1a3be701d1e98c3d4261d65cae3617fe5c7be
N315 <mixach333@gmail.com>
2022-09-15 16:15:27 +0300

    styles, forns, splashScreen added

--- app/src/main/java/com/mix333/androidquizapp/MainActivity.kt
+++ app/src/main/java/com/mix333/androidquizapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.mix333.androidquizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val isGone = MutableStateFlow(false)
+        // for fun and for future complex time-consuming starting logic
+        installSplashScreen().setKeepOnScreenCondition{
+            isGone.value
+        }
+        lifecycleScope.launch{
+            delay(1500)
+            isGone.value = true
+        }
         setContentView(R.layout.activity_main)
     }
 }
