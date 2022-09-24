a2a6e10777a8fac5baff1090b26871181136d2b4
N315 <mixach333@gmail.com>
2022-09-24 14:11:22 +0300

    architecture schema pre-defined

---
+++ app/src/main/java/com/mix333/androidquizapp/GameViewModel.kt
@@ -0,0 +1,6 @@
+package com.mix333.androidquizapp
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/mix333/androidquizapp/model/Question.kt
@@ -0,0 +1,6 @@
+package com.mix333.androidquizapp.model
+
+data class Question(
+    val text : String,
+    val answers : List<String>
+)
