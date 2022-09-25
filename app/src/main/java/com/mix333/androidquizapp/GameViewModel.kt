4b52a94611ac6fc7f2fac8c5add38402899181c5
N315 <mixach333@gmail.com>
2022-09-25 22:14:14 +0300

    Game Fragment and ViewModel fulfilled, TODO: change behaviour of displaying the text in fragment, questions are not shuffled so as answers

--- app/src/main/java/com/mix333/androidquizapp/GameViewModel.kt
+++ app/src/main/java/com/mix333/androidquizapp/GameViewModel.kt
@@ -1,9 +1,60 @@
 package com.mix333.androidquizapp
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mix333.androidquizapp.model.Question
 import com.mix333.androidquizapp.repository.ReceiverQuestionsListRepository
+import kotlinx.coroutines.launch
 
 class GameViewModel : ViewModel() {
     private val receiverQuestionsListRepository = ReceiverQuestionsListRepository()
-    private val questionsList = receiverQuestionsListRepository.receiveQuestions()
+    private lateinit var questionsList: MutableList<Question>
+    var currentQuestionNumber : Int = 1
+    lateinit var question : Question
+    lateinit var shuffledAnswers : List<String>
+    var questionsQuantity = -1
+    private var _gameWon = MutableLiveData<Boolean>()
+    val gameWon : LiveData<Boolean> = _gameWon
+
+    init {
+        viewModelScope.launch {
+            questionsList = receiverQuestionsListRepository.receiveQuestions()
+            question = questionsList[0]
+            shuffledAnswers = question.answers.shuffled()
+            questionsQuantity = receiveQuestionsCount()
+        }
+    }
+    private fun receiveQuestionsCount() : Int {
+        var questionsQuantity = (Math.random()*questionsList.size).toInt()
+        if(questionsQuantity<5) {questionsQuantity = 5}
+        return questionsQuantity
+    }
+
+
+    fun hasNextQuestionOrGameOver() : Boolean{
+        val result = currentQuestionNumber<questionsQuantity
+        if(result) {
+            currentQuestionNumber++
+        }else {
+            _gameWon.value = true
+        }
+        return result
+    }
+
+    fun updateQuestionTextAndAnswers(){
+        question = questionsList[currentQuestionNumber-1]
+        shuffledAnswers = question.answers.shuffled()
+    }
+
+    fun validateAnswer(answer: String){
+        if(answer.equals(question.answers[0])){
+            updateQuestionTextAndAnswers()
+        } else {
+            updateQuestionTextAndAnswers()
+        }
+    }
+
+
 }
--- app/src/main/java/com/mix333/androidquizapp/ui/GameFragment.kt
+++ app/src/main/java/com/mix333/androidquizapp/ui/GameFragment.kt
@@ -5,20 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mix333.androidquizapp.R
+import androidx.fragment.app.viewModels
+import com.mix333.androidquizapp.GameViewModel
 import com.mix333.androidquizapp.databinding.FragmentGameBinding
-import com.mix333.androidquizapp.databinding.FragmentTitleBinding
 
 class GameFragment : Fragment(){
     private var _binding : FragmentGameBinding? = null
     private val binding get() = _binding!!
+    private val viewModel : GameViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.questionText.text = viewModel.question.text
+        binding.radioButtonAnswer1.text = viewModel.question.answers[0]
+        binding.radioButtonAnswer2.text = viewModel.question.answers[1]
+        binding.radioButtonAnswer3.text = viewModel.question.answers[2]
+        binding.radioButtonAnswer4.text = viewModel.question.answers[3]
     }
 
     override fun onDestroyView() {
