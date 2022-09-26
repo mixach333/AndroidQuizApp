74213eecf1949228dc578f7a2be57123ffacd871
N315 <mixach333@gmail.com>
2022-09-26 17:02:12 +0300

    GameFragment radioGroup fixed

--- app/src/main/java/com/mix333/androidquizapp/ui/GameFragment.kt
+++ app/src/main/java/com/mix333/androidquizapp/ui/GameFragment.kt
@@ -1,10 +1,12 @@
 package com.mix333.androidquizapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.mix333.androidquizapp.GameViewModel
@@ -18,7 +20,7 @@ class GameFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
@@ -36,12 +38,22 @@ class GameFragment : Fragment() {
                 .navigate(R.id.action_gameFragment_to_gameOverFragment)
         }
         binding.submitAnswerButton.setOnClickListener {
-            when(binding.radioGroupAnswers.checkedRadioButtonId){
-                0 -> viewModel.validateAnswer(binding.radioButtonAnswer1.text.toString())
-                1 -> viewModel.validateAnswer(binding.radioButtonAnswer2.text.toString())
-                2 -> viewModel.validateAnswer(binding.radioButtonAnswer3.text.toString())
-                else -> viewModel.validateAnswer(binding.radioButtonAnswer4.text.toString())
+            val checkedId = binding.radioGroupAnswers.checkedRadioButtonId
+            val answerRadio = view.findViewById<RadioButton>(checkedId)
+            viewModel.validateAnswer(answerRadio.text.toString())
+            if(checkedId!=-1) {
+                when (checkedId) {
+                    binding.radioButtonAnswer1.id -> viewModel.validateAnswer(binding.radioButtonAnswer1.text.toString())
+                    binding.radioButtonAnswer2.id -> viewModel.validateAnswer(binding.radioButtonAnswer2.text.toString())
+                    binding.radioButtonAnswer3.id -> viewModel.validateAnswer(binding.radioButtonAnswer3.text.toString())
+                    else -> viewModel.validateAnswer(binding.radioButtonAnswer4.text.toString())
+                }
             }
+            Log.d("m333", "idChecked is ${binding.radioGroupAnswers.checkedRadioButtonId}")
+            Log.d("m333", "id1 is ${binding.radioButtonAnswer1.id}")
+            Log.d("m333", "id2 is ${binding.radioButtonAnswer2.id}")
+            Log.d("m333", "id3 is ${binding.radioButtonAnswer3.id}")
+            Log.d("m333", "id4 is ${binding.radioButtonAnswer4.id}")
             binding.radioGroupAnswers.clearCheck()
             updateQuestionsOnScreen()
         }
